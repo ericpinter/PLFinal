7878fd2a5dc2d1141b8569e53bafc94b0f80581f
ericpinter <48108495+ericpinter@users.noreply.github.com>
2021-05-01 16:28:51 -0500

    More Wave Types

--- Final-Project/src/Lib.hs
+++ Final-Project/src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Lib  
     ( someFunc
     ) where
@@ -5,14 +7,36 @@ module Lib
 import Data.WAVE
 import Data.List
 
+
 defaultHeader :: WAVEHeader 
 defaultHeader = WAVEHeader 2 48000 16 Nothing
 
-sinSamples :: Float -> Float -> [[WAVESample]]
-sinSamples sampleRate pitch = [ [round $ 200000000*sin(n*2*pi * pitch/sampleRate), something ]| n <- [1..100000], ]   
+instance Num a => Num [[a]] where
+    (+) = zipWith (zipWith (+))
+    (-) = zipWith (zipWith (-))
+    (*) = zipWith (zipWith (*))
+    negate = map (map negate)
+    abs    = map (map abs) 
+
+sawtooth :: Float -> Float -> Float -> WAVESample
+sawtooth sampleRate pitch n = round $ - 2 * 200000000 / pi * atan ( 1 / tan (n / 2 * pi * pitch/sampleRate))
+
+triangle :: Float -> Float -> Float -> WAVESample
+triangle sampleRate pitch n = round $ - 2 * 200000000 / pi * asin ( sin (n / 2 * pi * pitch/sampleRate))
+
+square :: Float -> Float -> Float -> WAVESample
+square sampleRate pitch n = (*200000000) $ round $ sin(n*2*pi * pitch/sampleRate)
+
+sine :: Float -> Float -> Float -> WAVESample
+sine sampleRate pitch n = round $ 200000000*sin(n*2*pi * pitch/sampleRate)
+
+
+samples :: (Float -> Float -> Float -> WAVESample) -> Float -> Float -> Float -> [[WAVESample]]
+samples f sampleRate pitch numSamples = [ [f sampleRate pitch n ] | n <- [0..numSamples]]
+
 
 w :: WAVE
-w = WAVE defaultHeader (transpose ( transpose (sinSamples 48000 440) ++ transpose(sinSamples 48000 470))) 
+w = WAVE defaultHeader ((samples triangle 48000 440 100000))
 
 someFunc :: IO ()
 someFunc = putWAVEFile "wave.wav" w
