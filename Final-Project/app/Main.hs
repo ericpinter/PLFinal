95fa69bcaf13f3481437c9efc02ca24a256d39a0
ericpinter <48108495+ericpinter@users.noreply.github.com>
2021-05-09 22:08:03 -0500

    Comments and slight tweaks

--- Final-Project/app/Main.hs
+++ Final-Project/app/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wall #-}
 module Main where
 
 import Lib
@@ -5,33 +6,34 @@ import Prelude hiding (sequence)
 
 main :: IO ()
 main = do 
-    --(saveWave "major.wav" (sequence triangle major) $ SoundSettings 2 48000 12 2 1)
-    --(saveWave "majorMicro.wav" (sequence triangle major) $ SoundSettings 2 48000 19 3 2)
-    --(saveWave "chromatic.wav" (sequence triangle chromatic) $ SoundSettings 2 48000 19 3 2)
-    (saveWave "blah.wav" blah $ SoundSettings 3 48000 12 2 1)
-    (saveWave "blahMono.wav" blahMono $ SoundSettings 1 48000 12 2 1)
+    saveWave "major.wav" (sequence triangle major) $ SoundSettings 2 48000 12 2 1
+    saveWave "majorMicro.wav" (sequence triangle major) $ SoundSettings 2 48000 19 3 2
+    saveWave "chromatic.wav" (sequence triangle chromatic) $ SoundSettings 2 48000 19 3 2
+    saveWave "channels.wav" showOffChannels $ SoundSettings 3 48000 12 2 1
+    saveWave "combine.wav" showOffCombine $ SoundSettings 1 48000 12 2 1
 
 major :: Sound Tune
-major ss@(SoundSettings _ _ _ whole half) = scanl (\ (n,o) step -> (n + step, o)) (0,4) [whole, whole, half, whole, whole, whole, half]  `zip` (repeat $ 1/2)
+major (SoundSettings _ _ _ whole half) = scanl (\ (n,o) step -> (n + step, o)) (0,4) [whole, whole, half, whole, whole, whole, half]  `zip` (repeat $ 1/2)
 
 chromatic :: Sound Tune
 chromatic (SoundSettings _ _ numSemis _ _) = take (round numSemis) $ (iterate (\(a,b) -> (a+1,b)) (0,4)) `zip` (repeat $ 1/4)
 
-blah :: Sound Wave
-blah = do 
+showOffChannels :: Sound Wave
+showOffChannels = do 
     l <- sequence sine maryHadALittleLamb
     r <- sequence square (transposeTune (-5) maryHadALittleLamb) 
     m <- sequence sawtooth (transposeTune (3) maryHadALittleLamb) 
     return $ channels [l,r,m]
 
-blahMono :: Sound Wave
-blahMono =  do 
+showOffCombine :: Sound Wave
+showOffCombine =  do 
     l <- sequence sine maryHadALittleLamb
     r <- sequence square (transposeTune (-5) maryHadALittleLamb) 
     m <- sequence sawtooth (transposeTune (3) maryHadALittleLamb) 
     return $ combine [l,r,m]
 
-
+rest :: Pitch
 rest = (0,-100)
 
+maryHadALittleLamb :: Sound Tune
 maryHadALittleLamb _ = [(5,4), (3,4), (1,4), (3,4), (5,4), (5,4), (5,4), rest, (3,4), (3,4), (3,4), rest, (5,4), (8,4), (8,4)] `zip` (repeat $ 1/4)
--- Final-Project/src/Lib.hs
+++ Final-Project/src/Lib.hs
@@ -2,36 +2,27 @@
 {-# OPTIONS_GHC -Wall #-}
 
 
-module Lib  
-    ( saveWave
-    , SoundSettings (SoundSettings)
-    , Sound
-    , Wave
-    , Tune
-    , sine
-    , square
-    , triangle
-    , sawtooth
-    , sequence
-    , channels
-    , combine
-    , transposeTune
-    ) where
+module Lib where
 import Prelude hiding (sequence)
 import Data.WAVE
 import Data.Int (Int32)
 
-
+-- This is similar to the prelude function zipWith, except that zipWith will stop combining the lists when one runs out of elements
+-- However if we want to express two sounds happening at the same time we don't want the longer one to be cut short
 zipWithLonger :: (a -> a -> a) -> [a] -> [a] -> [a]
 zipWithLonger f = zipLonger f id id 
 
+-- The channels function combines waves into different channels in a single wave.
+-- This can be used to assign what plays through the left and right speakers. 
+
+-- Note that you should be careful how many channels you use, as this should be the first number you give SoundSettings.
+-- Be careful about using channels mutiple times, since it can lead to the channels being split up in unexpected ways.
 channels :: [Wave] -> Wave
 channels = foldl (zipWithLonger (++)) []    
 
+-- Similar to channels, but overlays multiple waves ontop of each other in the same channel.
 combine :: [Wave] -> Wave
 combine = foldl (zipWithLonger (zipWithLonger (+))) []
-
-
 
 zipLonger :: (a -> b -> c) -> (a -> c) -> (b-> c) -> [a] -> [b] -> [c]
 zipLonger _ _ _ [] [] = []
@@ -39,12 +30,14 @@ zipLonger _ _ h [] bs = map h bs
 zipLonger _ g _ as [] = map g as 
 zipLonger f g h (a:as) (b:bs) = f a b : zipLonger f g h as bs
 
-                        --contains num channels, sample rate, number of semitones in the tuning, whole step, half step  
+                        --contains # of channels, sample rate, number of semitones in the tuning, whole step, half step  
 data SoundSettings = SoundSettings Int Float Float Int Int
 
+-- The type (Sound a) is just another way of talking about a function that returns an a for particular sound settings.
+-- Note that we can just ignore the settings, so any type a can be turned into a (Sound a) easily. 
 type Sound = (->) SoundSettings
 
-
+-- A Wave Function takes the frequency, amplitude, and phase and returns the sample of that wave at that particular phase.
 type WaveFunction = Float -> Float -> Float -> Sound WAVESample
 type Wave = [[WAVESample]]
 type Pitch = (Int,Int)
@@ -52,13 +45,16 @@ type Duration = Rational
 type Note = (Pitch,Duration)
 type Tune = [Note]
 
+-- The pitch (0,0) corresponds to C0 and is defined as the frequency 16.35
+-- Adding one to the second value transposes up an octave, via doubling the frequency
+-- Adding one to the first value transposes up a semitone. For example in a normal tuning with 12 semitones to an octave it entails multiplying the frequency by the twelfth root of two
 frequencyOf :: Pitch -> Sound Float
-frequencyOf (n, octave) (SoundSettings _ _ semitones _ _) =  16.35 * (2 ** ((fromIntegral n)/semitones + fromIntegral octave)) -- 16.35 is the frequency of C0. 
+frequencyOf (n, octave) (SoundSettings _ _ semitones _ _) =  16.35 * (2 ** ((fromIntegral n)/semitones + fromIntegral octave))
 
 maxAmplitude :: Float
 maxAmplitude = fromIntegral (maxBound :: Int32)
 
-
+-- There are four types of waves that are pretty simple to make using trigonometry
 sawtooth :: WaveFunction
 sawtooth freq amp n (SoundSettings _ sampleRate _ _ _) = round $ - 2 * (maxAmplitude * (2**amp) / pi) * atan ( 1 / tan (n / 2 * pi * freq/sampleRate))
 
@@ -71,25 +67,26 @@ square freq amp n (SoundSettings _ sampleRate _ _ _) = round $ (\a -> a * maxAmp
 sine :: WaveFunction
 sine freq amp n (SoundSettings _ sampleRate _ _ _) = round $ (maxAmplitude * (2**amp)) * sin(n*2*pi * freq/sampleRate)
 
-
+-- samples' takes an amplitude, type of wave, a frequency, and a duration and generates a wave with those properties
 samples' :: Float -> WaveFunction -> Float -> Duration -> Sound Wave
 samples' amp f freq beats ss@(SoundSettings _ sampleRate _ _ _) = [ [f freq amp n ss] | n <- [0.. sampleRate * fromRational beats]]
 
+-- samples just mirrors samples' with a default wave amplitude
 samples :: WaveFunction -> Float -> Duration -> Sound Wave
 samples = samples' (-2)
 
+-- Takes a type of wave and a Tune (i.e. a bunch of notes) and generates the wave that plays them sequentially.
 sequence :: WaveFunction -> Sound Tune -> Sound Wave
-sequence f notes = do 
-    notes'  <- notes
-    notes'' <- mapM (\(pitch,duration) -> do --lookup pitches based on the sound settings
-        freq <- frequencyOf pitch
-        return (freq, duration)) notes'
-    \ss -> foldl (\w (freq,duration) -> w ++ (samples f freq duration ss)) [[]] notes'' 
+sequence f st = do 
+    tune <- st
+    waves <- mapM (\(pitch,duration) -> do 
+        freq <- frequencyOf pitch     -- lookup frequencies based on the sound settings
+        samples f freq duration) tune -- get the wave associated with said frequency
+    return $ foldl (++) [] waves
 
-
+-- shifts each note in the tune up by a certain number of semitones
 transposeTune :: Int -> Sound Tune -> Sound Tune
-transposeTune semitones soundSettings = map (\((a,b),d) -> ((a+semitones,b),d)) . soundSettings
-
+transposeTune semitones tune = map (\((a,b),d) -> ((a+semitones,b),d)) . tune
 
 saveWave :: String -> Sound Wave -> Sound (IO ())
 saveWave fileName w ss@(SoundSettings numChannels sampleRate _ _ _) = putWAVEFile fileName $ WAVE (WAVEHeader numChannels (round sampleRate) 8 Nothing) $ w ss
